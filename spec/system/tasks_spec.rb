6960517b0d4992b23f303f18d1d7e198f1f984bf
keisuke713 <nebakei.tkb713@gmail.com>
2019-11-06 07:08:45 +0900

    Add system spec for create_task

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -11,7 +11,7 @@ describe 'Task', type: :system do
     end
   end
 
-  describe 'create_task' do
+  describe 'create_new_task' do
     context 'parameter is correct' do
       before do
         visit new_task_path
@@ -30,7 +30,51 @@ describe 'Task', type: :system do
     end
 
     context 'parameter is incorrect' do
-
+      before do
+        visit new_task_path
+        fill_in 'Title', with: 'MyTask'
+        fill_in 'Detail', with: ''
+        click_button
+      end
+      it 'display error message' do
+        expect(page).to have_title 'New Task'
+        expect(page).to have_text "Detail can't be blank"
+        expect(page).to have_button 'confirm'
+      end
+    end
+  end
+  describe 'confirm_new_task' do
+    context 'create_new_task' do
+      before do
+        visit new_task_path
+        fill_in 'Title', with: 'MyTask'
+        fill_in 'Detail', with: 'MyDetail'
+        click_button 'confirm'
+        click_button 'create'
+      end
+      it 'is registered' do
+        expect(page).to have_title 'Tasks'
+        expect(page).to have_content 'MyTask'
+        expect(page).to have_content 'MyDetail'
+        expect(page).to have_content '2019-11-06'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_link '編集する'
+      end
+    end
+    context 'back to create screen' do
+      before do
+        visit new_task_path
+        fill_in 'Title', with: 'MyTask'
+        fill_in 'Detail', with: 'MyDetail'
+        click_button 'confirm'
+        click_button 'back'
+      end
+      it 'back to create screen' do
+        expect(page).to have_title 'New Task'
+        expect(page).to have_content 'Title'
+        expect(page).to have_content 'Detail'
+        expect(page).to have_content 'Deadlie'
+      end
     end
   end
 end
