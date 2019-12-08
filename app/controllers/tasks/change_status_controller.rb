ca649a323d096d32a89afed99c024ef4d1dc2989
keisuke713 <nebakei.tkb713@gmail.com>
2019-12-08 16:32:19 +0900

    make it possible to change status to done

--- app/controllers/tasks/change_status_controller.rb
+++ app/controllers/tasks/change_status_controller.rb
@@ -1,5 +1,7 @@
 class Tasks::ChangeStatusController < ApplicationController
   def update
-    p 'update'
+    @task = Task.find(params[:id])
+    @task.update(status: 2)
+    redirect_to task_path @task.id
   end
 end
