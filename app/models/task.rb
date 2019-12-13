65b65682c5f1be3e1a77be89418ed30c15d7891d
keisuke713 <nebakei.tkb713@gmail.com>
2019-12-13 10:56:40 +0900

    Add import method for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,20 +9,30 @@ class Task < ApplicationRecord
   def created_by_current_user?(current_user)
     user_id == current_user&.id
   end
-
-  def self.fetch_instance_close_deadline(user)
-    user.tasks.select do |task|
-      task.deadline_is_today?
-    end
-  end
-
-  def self.fetch_instance_passed_deadline(user)
-    user.tasks.select do |task|
-      task.deadline < Date.today && task.status != 'done'
-    end
-  end
-
+  
   def deadline_is_today?
     self.deadline == Date.today
+  end
+
+  class << self
+    def fetch_instance_close_deadline(user)
+      user.tasks.select do |task|
+        task.deadline_is_today?
+      end
+    end
+
+    def fetch_instance_passed_deadline(user)
+      user.tasks.select do |task|
+        task.deadline < Date.today && task.status != 'done'
+      end
+    end
+
+    def import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+        task = new
+        task.attributes = row.to_hash.slice(*csv_attributes)
+        task.save!
+      end
+    end
   end
 end
