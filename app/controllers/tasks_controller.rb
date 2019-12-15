cb3afbf8676d444b7a444c409f4ed89bcbdee099
keisuke713 <nebakei.tkb713@gmail.com>
2019-12-15 12:53:54 +0900

    display success message when import csv

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,7 +69,8 @@ class TasksController < ApplicationController
 
   def import
     current_user.tasks.import(params[:file])
-    redirect_to root_path, flash[:success] = 'success in importing file' 
+    flash[:success] = 'インポートに成功しました。'
+    redirect_to root_path  
   end
 
   private
